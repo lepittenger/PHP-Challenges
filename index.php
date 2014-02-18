05dac24b085d58f719b8e7db8b9c785a19959a7d
Lauren Pittenger <lepittenger@gmail.com>
2014-02-17 20:09:56 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Idiot Box Survey</title>
+	<link rel='stylesheet' href='style.css' type='text/css'>
+	<?php require'logic.php'; ?>
+</head>
+<body>
+
+
+
+</body>
+</html>
